03153a4b474474fa9bec85fe4fcacf764b7ff91d
Kartik Thakur <kartikthakur029@gmail.com>
2023-11-09 13:37:06 +0530

    initial commit

---
+++ app/quiz/[id]/page.js
@@ -0,0 +1,28 @@
+'use client'
+import { Answer } from '@/components/Answer'
+import { Container } from '@/components/Container'
+import { getQuizQuestion } from '@/lib/quiz'
+import { useEffect, useState } from 'react'
+ 
+
+export default function QuizQuestion({ params }) {
+
+  const [question, setQuestion] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await getQuizQuestion(params.id)
+      let {question} = data
+      setQuestion(question);
+    }
+    fetchData();
+  }, []);
+  // const {question}= await getQuizQuestion(params.id)
+  return (
+    <Container as='main' className='flex flex-col gap-5 py-5'>
+      <h1 className='text-lg font-semibold'>{question?.title}</h1>
+      <Answer answers={question?.answers} questionId={params.id}/>
+
+    </Container>
+  )
+}
