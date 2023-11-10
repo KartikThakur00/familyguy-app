ad392fbbb1b7ef347f1acee1c70d56a5c58263bd
Kartik Thakur <kartikthakur029@gmail.com>
2023-11-10 12:03:38 +0530

    update

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,7 +5,7 @@ import { Container } from './Container'
 
 export const Navigation = () => {
   return (
-  <div className='sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50'>
+  <div className='sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.6)] border-b border-slate-800 z-50'>
     <Container className='flex justify-between py-5'>
       <Link href="/">
         <Image src="/logo.png" alt="Family Guy" width={70} height={50}/>
